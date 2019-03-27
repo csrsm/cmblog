e70bea0b1b78dc7a80ba89296f5205181794d196
csrsm <245129248@qq.com>
2019-03-27 22:20:20 +0800

    优化

---
+++ src/component/blogList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card } from 'antd';
+import { Icon } from 'antd';
+import '../App.css';
+
+function BlogList (props) {
+
+    let blog = props.blogList.map((blogList) => 
+    <Card hoverable 
+          className="card"      
+    >
+        <div key={blogList.id}>
+            <h2 className="item-title">
+                { blogList.title}
+            </h2>
+             <span > 
+                <Icon type="calendar"/>    
+                <time title="创建时间：2019-02-28 17:46:57" itemProp="dateCreated datePublished" dateTime="2019-02-28T17:46:57+00:00">2019-02-28</time>
+            </span> 
+        </div>
+        <p>
+            { blogList.content }
+        </p>
+    </Card>
+  );
+  
+  return (<div>{ blog }</div>);
+}
+
+export default BlogList;
